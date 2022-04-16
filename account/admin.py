e9a26c5b5b891df26c2430e2b3cfcba0dac3406f
Osayi Akoko <osayistreams@gmail.com>
2022-04-16 10:58:39 +0100

    init account app

---
+++ account/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .forms import UserCreationForm, UserChangeForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff',
+         'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'password1', 'password2', 'is_active'),
+        }),
+    )
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display_links = ('id', 'email')
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('first_name',)
+    filter_horizontal = ('groups', 'user_permissions',)
--- config/settings/common.py
+++ config/settings/common.py
@@ -44,7 +44,9 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = []
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    'account.apps.AccountConfig',
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
@@ -120,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'account.User'
