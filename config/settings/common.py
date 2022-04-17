1a98f3b62a0a407d1fb5ae973a21181957c628a7
Osayi Akoko <osayistreams@gmail.com>
2022-04-17 23:04:35 +0100

    init customer app

--- config/settings/common.py
+++ config/settings/common.py
@@ -52,6 +52,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'core.apps.CoreConfig',
     'account.apps.AccountConfig',
+    'customer.apps.CustomerConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
